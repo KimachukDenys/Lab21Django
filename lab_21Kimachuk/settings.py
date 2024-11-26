5d503e938b268585fbf7f289e21d9a615413f12f
KimachukDenys <youyou777333@gmail.com>
2024-11-26 14:31:12 +0200

    Додано PostgreSQL, та були додані користувачі в адмін панелі

--- lab_21Kimachuk/settings.py
+++ lab_21Kimachuk/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lab_21Kimachuk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'labdjangotest',
+        'USER': 'djangoadmin',
+        'PASSWORD': 'postgresql',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
